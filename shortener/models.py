a58a287aa5e49fc3e2c07f6f300bcc2b03413349
Pedro Miguel <pedro.miguel@live.co.uk>
2023-06-13 22:24:47 +0100

    Refactor models to make use of the MD5 hash algorithm when shortening URLs

--- shortener/models.py
+++ shortener/models.py
@@ -1,35 +1,32 @@
+from hashlib import md5
+
 from django.db import models
 
-from .utils import code_generator
+
+class URLManager(models.Model):
+    def all(self):
+        qs = super(URLManager, self).all(*args, **kwargs)
+        qs = qs.filter(active=True)
+        return qs
 
 
 class URL(models.Model):
-    full_url = models.URLField(max_length=220)
-    short_generated_url = models.CharField(
-        max_length=15,
-        unique=True,
-        blank=True
-    )
-    updated = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    full_url = models.URLField(unique=True)
+    url_hash = models.URLField(unique=True)
+    clicks = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def create_shortcode(self, size=6):
-        new_code = code_generator(size=size)
-        short_url_exists = self.__class__.objects.filter(
-            short_generated_url=new_code
-        ).exists()
-
-        if short_url_exists:
-            return self.create_shortcode(size=6)
-        return new_code
+    def clicked(self):
+        self.clicks += 1
+        self.save()
 
     def save(self, *args, **kwargs):
-        if self.short_generated_url is None or self.short_generated_url == "":
-            self.short_generated_url = self.create_shortcode()
-        super(URL, self).save(*args, **kwargs)
+        if not self.id:
+            self.url_hash = md5(self.full_url.encode()).hexdigest()[:10]
+        return super().save(*args, **kwargs)
 
     def __str__(self):
-        return str(self.full_url)
+        return str(self.url_hash)
 
     def __unicode__(self):
-        return str(self.full_url)
+        return str(self.url_hash)
